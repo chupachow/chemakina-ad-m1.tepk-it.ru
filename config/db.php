f7a5c0b73ea42e7e4d62046e3bdef83cc4a6eca5
Чемакина Алёна Дмитриевна <chemakina-ad@college.tepk.dev>
2025-06-04 13:58:49 +0700

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=chemakina-ad_m1',
+    'username' => 'chemakina-ad',
+    'password' => 'PassWord623!',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
